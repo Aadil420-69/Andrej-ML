27596cb6ec2da4727041ee81a0eb40fe8d1e0525
AadilGoyal <aadilgoyal1@gmail.com>
2024-10-22 00:43:32 +0530

    GPT (part 3 - Single and Multi Head Attention)

--- GPT/v2.py
+++ GPT/v2.py
@@ -14,9 +14,9 @@ import torch.nn.functional as F
 # hyperparameters
 batch_size = 32	# how many independent processes will we process in parallel
 block_size = 8	# whar is the maximum context length for prediction
-max_iters = 3000
-eval_intervals = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_intervals = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
@@ -90,6 +90,49 @@ def estimate_loss():
 	model.train()	# sets model into train mode
 	return out
 
+class Head(nn.Module):
+	"""_summary_
+	one head of self-attention
+	"""
+	def __init__(self, head_size):
+		super().__init__()
+		self.key = nn.Linear(n_embd, head_size, bias=False)
+		self.query = nn.Linear(n_embd, head_size, bias=False)
+		self.value = nn.Linear(n_embd, head_size, bias=False)
+		self.register_buffer('tril', torch.tril(torch.ones((block_size, block_size))))
+	
+	def forward(self, x: torch.Tensor):
+		"""_summary_
+		forward passes the head
+
+		Args:
+			x (torch.Tensor): the activations of the different neurons on which the "affinity" has to be calculated
+
+		Returns:
+			torch.Tensor: the "affinities" of the neurons
+		"""
+		B, T, C = x.shape
+		k: torch.Tensor = self.key(x)    # B, T, C
+		q: torch.Tensor = self.query(x)    # B, T, C
+
+		# compute attention scores ("affinities")
+		wei: torch.Tensor = q @ k.transpose(-2, -1) * C**-0.5    # (B, T, C) @ (B, C, T) --> (B, T, T)
+		wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))    # (B, T, T)
+		wei = F.softmax(wei, dim=-1)    # (B, T, T)
+		# perform the weighted aggregation of the values
+		v = self.value(x)    # (B, T, C)
+		out = wei @ v    # (B, T, T) @ (B, T, C) --> (B, T, C)
+		return out
+
+
+class MultiHeadAttention(nn.Module):
+	def __init__(self, num_heads, head_size):
+		super().__init__()
+		self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+	
+	def forward(self, x):
+		return torch.cat([h(x) for h in self.heads], dim=-1)
+
 
 class BigramLanguageModel(nn.Module):
 	"""_summary_
@@ -101,6 +144,7 @@ class BigramLanguageModel(nn.Module):
 		# each token directly reads off the logits for the next token in the lookup table
 		self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
 		self.position_embedding_table = nn.Embedding(block_size, n_embd)
+		self.sa_heads = MultiHeadAttention(4, n_embd // 4)
 		self.lm_head = nn.Linear(n_embd, vocab_size)
 
 	def forward(self, idx, targets=None):
@@ -119,8 +163,8 @@ class BigramLanguageModel(nn.Module):
 		tok_emb = self.token_embedding_table(idx)    # (B, T, C)
 		pos_emb = self.position_embedding_table(torch.arange(T, device=device))    # (T, C)
 		x = tok_emb + pos_emb    # (B, T, C)
-		logits = self.lm_head(x)
-
+		x = self.sa_heads(x)    # apply one head of self-attention, (B, T, C)
+		logits = self.lm_head(x)    # (B, T, vocab_size)
 
 		if targets is None:
 			loss = None
@@ -145,8 +189,10 @@ class BigramLanguageModel(nn.Module):
 		"""
 		# idx is (B, T) array of indices in the current context
 		for _ in range(max_new_tokens):
+			# crop idx to the last block_size token
+			idx_cond = idx[:, -block_size:]
 			# get the Predicitons
-			logits, __ = self(idx)
+			logits, __ = self(idx_cond)
 			# focus only on the last T step
 			logits = logits[:, -1, :]	# becomes (B, C)
 			# apply softmax to get probabilities
